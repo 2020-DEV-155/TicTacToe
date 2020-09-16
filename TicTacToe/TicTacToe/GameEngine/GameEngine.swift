6f5e4a671ebc41a8bcd34e30698760e50ce37d37
DEV - 155 <2020dev155@gmail.com>
2020-09-16 18:45:12 +0100

    Added business logic and tests for first move

--- TicTacToe/TicTacToe/GameEngine/GameEngine.swift
+++ TicTacToe/TicTacToe/GameEngine/GameEngine.swift
@@ -5,7 +5,22 @@ enum Turn {
 	case player2
 }
 
+protocol GameEngineDelegate {
+
+	func played(at row: Int, column: Int)
+}
+
 final class GameEngine {
 
+	let delegate: GameEngineDelegate
+
 	private(set) var currentTurn: Turn = .player1
+
+	init(delegate: GameEngineDelegate) {
+		self.delegate = delegate
+	}
+
+	func play(atRow row: Int, column: Int) {
+		delegate.played(at: row, column: column)
+	}
 }
--- TicTacToe/TicTacToeTests/GameEngine/GameEngineTests.swift
+++ TicTacToe/TicTacToeTests/GameEngine/GameEngineTests.swift
@@ -11,9 +11,36 @@ class GameEngineTests: XCTestCase {
 
 	func test_WhenInitialised_ThenPlayer1HasTurn() {
 		// given & when
-		sut = GameEngine()
+		sut = GameEngine(delegate: MockDelegate())
 
 		// then
 		XCTAssertEqual(sut.currentTurn, .player1)
+	}
+
+	func test_WhenMoveIsPlayed_ThenDelegateIsCalled() {
+		// given
+		let delegate = MockDelegate()
+		sut = GameEngine(delegate: delegate)
+
+		// when
+		sut.play(atRow: 0, column: 0)
+
+		// then
+		XCTAssertTrue(delegate.isPlayCalled)
+		XCTAssertEqual(delegate.playedColumn, 0)
+		XCTAssertEqual(delegate.playedRow, 0)
+	}
+}
+
+class MockDelegate: GameEngineDelegate {
+
+	private(set) var isPlayCalled = false
+	private(set) var playedRow: Int?
+	private(set) var playedColumn: Int?
+
+	func played(at row: Int, column: Int) {
+		isPlayCalled = true
+		playedRow = row
+		playedColumn = column
 	}
 }
