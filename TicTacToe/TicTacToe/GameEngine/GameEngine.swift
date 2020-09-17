a945dc496c09a28f903ac09950ebeca6fcbef221
DEV - 155 <2020dev155@gmail.com>
2020-09-17 11:21:37 +0100

    added tests when a move is made, the box is occupied with the player symbol
    - Also, refactored Player enum to reflect player number and created a separate enum for the Symbols.

--- TicTacToe/TicTacToe/GameEngine/GameEngine.swift
+++ TicTacToe/TicTacToe/GameEngine/GameEngine.swift
@@ -1,8 +1,14 @@
 import Foundation
 
-enum Player {
-	case cross
+enum Player: Int {
+	case first = 0
+	case second = 1
+}
+
+enum Symbols {
+	case empty
 	case nought
+	case cross
 }
 
 protocol GameEngineDelegate {
@@ -14,18 +20,26 @@ final class GameEngine {
 
 	let delegate: GameEngineDelegate
 
-	private(set) var currentTurn: Player = .cross
+	private(set) var currentTurn: Player = .first
+	private(set) var boxes: [[Symbols]]
 
 	init(delegate: GameEngineDelegate) {
 		self.delegate = delegate
+		boxes = Array(repeating: Array(repeating: .empty, count: 3), count: 3)
 	}
 
 	func play(atRow row: Int, column: Int) {
+
+		// Make the move
+		boxes[row][column] = (currentTurn == .first) ? .cross : .nought
+
+		// Update the delegate
 		delegate.played(at: row, column: column, by: currentTurn)
 
+		// Switch the turn
 		switch currentTurn {
-		case .cross: currentTurn = .nought
-		case .nought: currentTurn = .cross
+		case .first: currentTurn = .second
+		case .second: currentTurn = .first
 		}
 	}
 }
--- TicTacToe/TicTacToeTests/GameEngine/GameEngineTests.swift
+++ TicTacToe/TicTacToeTests/GameEngine/GameEngineTests.swift
@@ -14,7 +14,7 @@ class GameEngineTests: XCTestCase {
 		sut = GameEngine(delegate: MockDelegate())
 
 		// then
-		XCTAssertEqual(sut.currentTurn, .cross)
+		XCTAssertEqual(sut.currentTurn, .first)
 	}
 
 	func test_WhenFirstMoveIsPlayed_ThenDelegateIsCalledAndFirstTurnIsCross() {
@@ -29,10 +29,10 @@ class GameEngineTests: XCTestCase {
 		XCTAssertTrue(delegate.isPlayCalled)
 		XCTAssertEqual(delegate.playedRow, 0)
 		XCTAssertEqual(delegate.playedColumn, 0)
-		XCTAssertEqual(delegate.by, .cross)
+		XCTAssertEqual(delegate.by, .first)
 	}
 
-	func test_WhenTwoMovesArePlayed_ThenSecondTurnIsIsNought() {
+	func test_WhenTwoMovesArePlayed_ThenSecondTurnIsNought() {
 		// given
 		let delegate = MockDelegate()
 		sut = GameEngine(delegate: delegate)
@@ -45,7 +45,44 @@ class GameEngineTests: XCTestCase {
 		XCTAssertTrue(delegate.isPlayCalled)
 		XCTAssertEqual(delegate.playedRow, 1)
 		XCTAssertEqual(delegate.playedColumn, 1)
-		XCTAssertEqual(delegate.by, .nought)
+		XCTAssertEqual(delegate.by, .second)
+	}
+
+	func test_WhenNoMovesArePlayed_ThenTheBoxesAreEmpty() {
+		// given
+		sut = GameEngine(delegate: MockDelegate())
+
+		// when - no moves are played
+
+		// then
+		XCTAssertEqual(sut.boxes[0][0], .empty)
+		XCTAssertEqual(sut.boxes[0][1], .empty)
+		XCTAssertEqual(sut.boxes[0][2], .empty)
+		XCTAssertEqual(sut.boxes[1][0], .empty)
+		XCTAssertEqual(sut.boxes[1][1], .empty)
+		XCTAssertEqual(sut.boxes[1][2], .empty)
+		XCTAssertEqual(sut.boxes[2][0], .empty)
+		XCTAssertEqual(sut.boxes[2][1], .empty)
+		XCTAssertEqual(sut.boxes[2][2], .empty)
+	}
+
+	func test_WhenFirstMoveIsPlayed_ThenOnlyTheSpecifiedBoxIsFilled() {
+		// given
+		sut = GameEngine(delegate: MockDelegate())
+
+		// when
+		sut.play(atRow: 0, column: 1)
+
+		// then
+		XCTAssertEqual(sut.boxes[0][0], .empty)
+		XCTAssertEqual(sut.boxes[0][1], .cross)
+		XCTAssertEqual(sut.boxes[0][2], .empty)
+		XCTAssertEqual(sut.boxes[1][0], .empty)
+		XCTAssertEqual(sut.boxes[1][1], .empty)
+		XCTAssertEqual(sut.boxes[1][2], .empty)
+		XCTAssertEqual(sut.boxes[2][0], .empty)
+		XCTAssertEqual(sut.boxes[2][1], .empty)
+		XCTAssertEqual(sut.boxes[2][2], .empty)
 	}
 }
 
